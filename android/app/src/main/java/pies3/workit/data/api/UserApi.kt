72f8d172656a713a63a764850b64935e1743929d
Guilherme Borges <guilhermepereira@alu.ufc.br>
2026-01-11 19:39:52 -0300

    feat: add updateUser to UserApi

--- android/app/src/main/java/pies3/workit/data/api/UserApi.kt
+++ android/app/src/main/java/pies3/workit/data/api/UserApi.kt
@@ -1,10 +1,19 @@
 package pies3.workit.data.api
 
+import pies3.workit.data.dto.user.UpdateUserRequest
 import pies3.workit.data.dto.user.UserResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface UserApi {
     @GET("users/{id}")
     suspend fun getUserById(@Path("id") id: String): UserResponse
+
+    @PUT("users/{id}")
+    suspend fun updateUser(
+        @Path("id") id: String,
+        @Body request: UpdateUserRequest
+    ): UserResponse
 }
