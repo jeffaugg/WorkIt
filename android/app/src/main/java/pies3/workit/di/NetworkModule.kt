954e3f8d3a7ee97d899a233d0b47debc90843a3c
Guilherme Borges <guilhermepereira@alu.ufc.br>
2026-01-10 21:04:51 -0300

    feat: add auth interceptor to OkHttpClient

--- android/app/src/main/java/pies3/workit/di/NetworkModule.kt
+++ android/app/src/main/java/pies3/workit/di/NetworkModule.kt
@@ -7,6 +7,7 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import pies3.workit.data.api.AuthApi
+import pies3.workit.data.interceptors.AuthInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -29,9 +30,11 @@ object NetworkModule {
     @Provides
     @Singleton
     fun provideOkHttpClient(
-        loggingInterceptor: HttpLoggingInterceptor
+        loggingInterceptor: HttpLoggingInterceptor,
+        authInterceptor: AuthInterceptor
     ): OkHttpClient {
         return OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
             .addInterceptor(loggingInterceptor)
             .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
