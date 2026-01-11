cae0bfd665c38eb65f6ae795ae9f3acfe4028ae5
Guilherme Borges <guilhermepereira@alu.ufc.br>
2026-01-11 09:00:45 -0300

    feat: add GroupsRepository for creating groups

---
+++ android/app/src/main/java/pies3/workit/data/repository/GroupsRepository.kt
@@ -0,0 +1,20 @@
+package pies3.workit.data.repository
+
+import pies3.workit.data.api.GroupApi
+import pies3.workit.data.dto.group.CreateGroupResponse
+import pies3.workit.data.dto.group.CreateGroupRequest
+import javax.inject.Inject
+
+
+class GroupsRepository @Inject constructor(
+    private val groupApi: GroupApi
+) {
+    suspend fun createGroup(name: String, description: String?, imageUrl: String?): Result<CreateGroupResponse> {
+        return try {
+            val response = groupApi.create(CreateGroupRequest(name, description, imageUrl))
+            Result.success(response)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+}
