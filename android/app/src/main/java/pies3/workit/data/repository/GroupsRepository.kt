81ce57042bc4dee5c4712b16026bb477cd083a25
Guilherme Borges <guilhermepereira@alu.ufc.br>
2026-01-11 11:16:59 -0300

    feat: implement getGroups in GroupsRepository

--- android/app/src/main/java/pies3/workit/data/repository/GroupsRepository.kt
+++ android/app/src/main/java/pies3/workit/data/repository/GroupsRepository.kt
@@ -3,6 +3,7 @@ package pies3.workit.data.repository
 import pies3.workit.data.api.GroupApi
 import pies3.workit.data.dto.group.CreateGroupResponse
 import pies3.workit.data.dto.group.CreateGroupRequest
+import pies3.workit.data.dto.group.GroupListResponse
 import javax.inject.Inject
 
 
@@ -12,6 +13,15 @@ class GroupsRepository @Inject constructor(
     suspend fun createGroup(name: String, description: String?, imageUrl: String?): Result<CreateGroupResponse> {
         return try {
             val response = groupApi.create(CreateGroupRequest(name, description, imageUrl))
+            Result.success(response)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun getGroups(): Result<List<GroupListResponse>> {
+        return try {
+            val response = groupApi.getGroups()
             Result.success(response)
         } catch (e: Exception) {
             Result.failure(e)
