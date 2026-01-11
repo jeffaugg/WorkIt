d8214034891e4e3f111b31b9fb60a342b0b90afe
Guilherme Borges <guilhermepereira@alu.ufc.br>
2026-01-11 11:16:44 -0300

    feat: add endpoint to fetch group list

--- android/app/src/main/java/pies3/workit/data/api/GroupApi.kt
+++ android/app/src/main/java/pies3/workit/data/api/GroupApi.kt
@@ -2,10 +2,15 @@ package pies3.workit.data.api
 
 import pies3.workit.data.dto.group.CreateGroupRequest
 import pies3.workit.data.dto.group.CreateGroupResponse
+import pies3.workit.data.dto.group.GroupListResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface GroupApi{
     @POST("groups")
     suspend fun create(@Body request: CreateGroupRequest): CreateGroupResponse
+
+    @GET("groups")
+    suspend fun getGroups(): List<GroupListResponse>
 }
